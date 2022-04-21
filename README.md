d899f02a38cb11da2aa16b0e2cd8f29c00442354
agus-a97 <agustin.f.aguilar97@gmail.com>
2022-04-21 15:47:25 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-7pjjnk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-7pjjnk)
---
+++ src/app/products.ts
@@ -0,0 +1,33 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    image: 'https://http.cat/101'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    image: 'https://http.cat/102'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+    image: 'https://http.cat/103'
+  },
+  {
+    name: 'Phone Premium',
+    price: 1099,
+    description: '',
+    image: 'https://http.cat/104'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
